90adea2ef9930515ef30cdcb30f8250fdd6f769f
jordila5108 <jordy_fvs_5108@hotmail.com>
2022-08-08 16:37:46 -0500

    primer commit

--- pivot_table.py
+++ pivot_table.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame({"Author": ["joseluna", "sixt93", "alberto", "carlosz", "jgbesq",
+empleados = {"Author": ["joseluna", "sixt93", "alberto", "carlosz", "jgbesq",
                               "nuri90", "santiago8", "joseluna", "alberto", "jgbesq",
                               "sixt93", "santiago8", "santiago8", "nico23", "alberto"],
                    "Sentiment": ["positive", "negative", "positive", "negative", "negative",
@@ -10,21 +10,13 @@ df = pd.DataFrame({"Author": ["joseluna", "sixt93", "alberto", "carlosz", "jgbes
                                  "neutral", "neutral", "neutral", "positive"],
                    "country": ["Ecuador", "Colombia", "Francia", "USA", "UK", "Spain", "Chile",
                                "Peru", "Francia", "Panama", "Portugal", "Chile", "Bolivia", "USA", "USA"],
-                   "Theme": ["PA", "CA", "OT", "OT", "PA", "EV", "KS", "KS", "OT", "KS", "OT", "KS", "PA", "PA", "KS"]})
+                   "Theme": ["PA", "CA", "OT", "OT", "PA", "EV", "KS", "KS", "OT", "KS", "OT", "KS", "PA", "PA", "KS"]}
 
+df = pd.DataFrame(empleados, columns=['Author', 'Sentiment', 'country', 'Theme'])
 
-table = pd.pivot_table(df, values='Theme', index=['Author', 'Sentiment'],
-                    columns=['country'], aggfunc=np.sum)
+pivot = df.pivot_table(index=['Author', 'Sentiment'], values=['country', 'Theme'], aggfunc='sum')
 
-
-table1 = pd.pivot_table(df, values='Theme', index=['Author', 'Sentiment'],
-                    columns=['country'], aggfunc=np.sum, fill_value=0)
-
-
-
-
-
-
+print(pivot)
 
 
 
